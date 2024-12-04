6f44434d2e93e9b22f6f7a0af9dfc3d01b6d8c7d
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2024-12-04 21:28:41 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-nasm -f elf32 src/kernel/boot.asm -o kasm.o
-gcc -fno-stack-protector -m32 -c src/kernel/kernel.cpp -o kmain.o
-ld -m elf_i386 -T linker.ld -o kernel kasm.o kmain.o
-rm kasm.o kmain.o
-mv kernel build/boot/kernel.bin
-grub-mkrescue -o build.iso build
-rm build/boot/kernel.bin #build/boot/kernel.img
